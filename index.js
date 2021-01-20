a482a3e58d43d50def97ec684619e362309bcbde
sheilagomes <sheilagomes1@gmail.com>
2021-01-19 23:06:31 -0300

    Corrigido o botão show more

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const navToggle = document.querySelector('.nav__toggle');
+const showProjects = document.querySelector('.show-more');
+// const featuredProjects = document.querySelector('.project-tile');
+const featuredProjects = Array.from(document.querySelectorAll('.project-tile'));
+
 
 navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
+	document.body.classList.toggle('nav-open');
+});
+
+showProjects.addEventListener('click', () => {
+	if (showProjects.innerHTML == `show less`){
+		showProjects.innerHTML = `show more`
+		featuredProjects.forEach(tile => tile.classList.toggle('show-less'));
+	}
+	else {
+		showProjects.innerHTML = `show less`
+		featuredProjects.forEach(tile => tile.classList.toggle('show-less'))
+	}
 });
